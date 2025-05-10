05edeb51ad2485fba3868d845c65e3001adc330c
Klassik <cordisdi@yandex.ru>
2025-05-10 14:29:14 +0400

    stepik

--- new_file.py
+++ new_file.py
@@ -1,3 +1,5 @@
-x=1
-y=5
-# print(x+y)
+print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+
+print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+
+print("Honey, what's your hurry", end='?')
